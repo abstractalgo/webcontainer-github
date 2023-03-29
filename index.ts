5ccbeccb414424e22ce843f9b512c88554d8137b
Dragan Okanovic <dragan.okan@gmail.com>
2023-03-29 18:11:55 +0200

    cleanup

--- index.ts
+++ index.ts
@@ -3,22 +3,17 @@ import { WebContainer } from "@webcontainer/api";
 import { getGithubFilesTree } from "./gh";
 
 async function main() {
+  // get files from github
   const files = await getGithubFilesTree({
     owner: "foursquare",
     repo: "unfolded-sdk-examples",
     path: "map-sdk/v1/react/react-basic-example",
   });
 
-  // First we boot a WebContainer
+  // setup webcontainer
   const webcontainer = await WebContainer.boot();
-
-  // After booting the container we copy all of our project files
-  // into the container's file system
-  const res = await webcontainer.mount(files);
-
-  // Once the files have been mounted, we install the project's
-  // dependencies by spawning `npm install`
-  const install = await webcontainer.spawn("yarn", ["install"]);
+  await webcontainer.mount(files);
+  const install = await webcontainer.spawn("npm", ["install"]);
   install.output.pipeTo(
     new WritableStream({
       write(data) {
@@ -26,18 +21,12 @@ async function main() {
       },
     })
   );
-
-  const installCode = await install.exit;
-  if (installCode !== 0) {
-    throw new Error("Installation failed");
-  }
-
-  // Once all dependencies have been installed, we can spawn `npm`
-  // to run the `dev` script from the project's `package.json`
-  await webcontainer.spawn("yarn", ["dev"]);
-
+  await install.exit;
+  await webcontainer.spawn("npm", ["run", "dev"]);
   webcontainer.on("server-ready", (port, url) => {
-    const iframe = document.getElementById("ram");
+    const iframe = document.getElementsByTagName(
+      "iframe"
+    )[0] as HTMLIFrameElement;
     iframe.src = url;
   });
 }
